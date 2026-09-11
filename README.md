ac6335c560446e6ebac0d7c68e99d9cefbbd7532
Dr-Verdin <piscioneri24122004@gmail.com>
2026-09-11 20:22:58 -0300

    criei as structs em simulation.h

---
+++ README.md
@@ -0,0 +1,15 @@
+include/: guarda os arquivos .h (interfaces/declarações)
+    - input.h
+    - output.h
+
+src/: implementação das funções
+    - input.c: implementação da leitura/inicialização
+    - output.c: implementação da impressão
+
+tests/: casos de teste
+
+scripts/: scripts para automatizar tarefas talvez (run_tests.sh, compare.sh, benchmark.sh, ...)
+
+results/: guardar os resultados dos experimentos talvez (tempos.csv, speedup.csv, ...)
+
+report/: relatorio final (relatorio.pdf)
---
+++ include/simulation.h
@@ -0,0 +1,75 @@
+// Config → parâmetros da simulação;
+// Cell → cobertura e umidade;
+// Zone → informações das zonas;
+// Simulation → todas as estruturas necessárias durante a simulação;
+// Results → resultados que serão impressos no final;
+// constantes para os códigos de cobertura e estado.
+
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+// Códigos das coberturas:
+#define COBERTURA_AGUA 0
+#define COBERTURA_SOLO 1
+#define COBERTURA_VEGETACAO 2
+#define COBERTURA_FLORESTA 3
+
+// Códigos dos estados:
+#define ESTADO_NAO_COMBUSTIVEL 0
+#define ESTADO_INTACTA 1
+#define ESTADO_CHAMAS 2
+#define ESTADO_QUEIMADA 3
+#define ESTADO_CONTENCAO 4
+
+#endif // SIMULATION_H
+
+typedef struct Config
+{
+    // Primeira linha:
+    int L;             // número de linhas da matriz
+    int c;             // número de colunas da matriz
+    int P;             // número de passos da simulação
+    int T;             // número de threads
+    unsigned int seed; // semente para geração de números aleatórios
+    float LIMIAR;      // limiar de propagação do fogo
+
+    // Segunda linha:
+    int vento_linha;  // direção do vento na linha
+    int vento_coluna; // direção do vento na coluna
+    int V;            // intensidade do vento
+
+    // Terceira linha:
+    int F; // número de focos de incêndio
+    int Z; // número de zonas de contenção
+} Config;
+
+typedef struct Cell
+{
+    int cobertura; // código da cobertura: valoro nome da variavel?
+    int umidade;   // umidade do terreno
+} Cell;
+
+typedef struct
+{
+    int passo_ativacao; // passo em que a zona foi ativada
+    int linha_inicial;  // linha inicial da zona
+    int coluna_inicial; // coluna inicial da zona
+    int linha_final;    // linha final da zona
+    int coluna_final;   // coluna final da zona
+} Zone;
+
+typedef struct
+{
+    Config config;  // parâmetros da simulação
+    Cell *cells;    // informações de cada célula (cobertura e umidade)
+    Zone *zonas;    // vetor contendo as zonas de contenção
+
+    int *estado_atual;   // estado atual de cada célula
+    int *proximo_estado; // próximo estado de cada célula
+
+    int *tempo_atual;   // tempo de queima restante no passo atual
+    int *proximo_tempo; // tempo de queima que será calculado para o próximo passo
+
+    int *ativacao;                              // passo de ativação da zona que cobre cada célula; -1 indica que nenhuma zona cobre a célula
+    long long celulas_combustiveis_iniciais;    // número de células inicialmente combustíveis (vegetação + floresta)
+} Simulation;
